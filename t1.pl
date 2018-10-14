5c2ef9f122dbe439e1e646bef18bbcb0c6aec26f
BuildTools <unconfigured@null.spigotmc.org>
2018-10-14 11:26:52 -0600

    This code will now take a commandline parameter, to describe the maximum product of any question.
    You can still call it like this:
    ./t1.pl (and it will produce questions with maximum anwer of 42)
    
    But now, you can also call it like this:
    ./t1.pl 49

--- t1.pl
+++ t1.pl
@@ -1,20 +1,19 @@
 #!/usr/bin/perl -w 
 use strict; 
-#
-#
-# print out questions
+# print questions to STDOUT
 
 # Generate two real, random numbers: 
 my $productsTo = 42;
+$productsTo = $ARGV[0] if defined $ARGV[0];
 my $roundDownTo = 2;
 print "Products to $productsTo\n\n";
-my ($counter, $randA, $randB ); 
-for ($counter = 1; $counter<31; $counter++) {
+my ($maxQuestions, $randA, $randB ); 
+for ($maxQuestions = 1; $maxQuestions<31; $maxQuestions++) {
 	do {
 		$randA  = int(rand($productsTo));
 		$randB  = int(rand($productsTo));
 	} while (($randA * $randB >= $productsTo) || $randA < $roundDownTo || $randB < $roundDownTo);
 
-	print "$counter. $randA x $randB =\n";
+	print "$maxQuestions. $randA x $randB =\n";
 
 }
