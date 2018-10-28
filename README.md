553afca08c20515c3549fd691c7b2e1e70235ba8
BuildTools <unconfigured@null.spigotmc.org>
2018-10-28 11:56:38 -0600

    Now upgraded to add division questions

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 From a Linux desktop, I use this crontab to generate a test, with a little lpr magic to print it, at the same time 
 my kids get home from school -- the tests are waiting for him when he gets home.
 
-12 15 * * * /home/nmacgreg/homework/Maxwell/timeTest/t1.pl | lpr -o cpi=6 -o lpi=4
+12 15 * * * /home/nmacgreg/homework/Maxwell/timeTest/t2.pl --maxProduct=28| lpr -o cpi=6 -o lpi=4
+12 15 * * * /home/nmacgreg/homework/Maxwell/timeTest/t2.pl --maxProduct=49 --maxDiv=12 | lpr -o cpi=6 -o lpi=4
 
-
+Oct 18, 2018
 Until this week, his goal was "products up to 32", which I think was a goal for Grade 3.
 
 But this week, the goalposts also "changed shape", as the target is now stated as "Most Challenging Question: 7x7", 
 and looking ahead in the rubric, the goal for this year is clearly multiplication & division, up to 12.
+
+
+Oct 25, 2018
+This week's goal is "Most challenging question: 7x7 &  12/4"
+This week, we're adding division questions, controllable with the new maxDiv parameter.
+Is this a bug?  All questions less than or equal to the new "--maxDiv" parameter are printed as division questions.
+Eventually this will be a problem. 
--- t2.pl
+++ t2.pl
@@ -1,23 +1,34 @@
 #!/usr/bin/perl -w 
 # Purpose: print math question for Grade 5 "Timed Tests"
-use strict; 
+
 #use taint; 
+use strict; 
+use Getopt::Long; 
+my $maxProduct = 42;  						# Declare a variable & set a default
+my $maxDiv;
+GetOptions (	"maxProduct=i"	=> \$maxProduct, 
+		"maxDiv=i"	=> \$maxDiv
+	);
+#$maxProduct = $ARGV[0] if defined $ARGV[0]; 			# Read from the commandline
 
 # Generate two real, random numbers: 
-my $productsTo = 42;  						# Declare a variable & set a default
-$productsTo = $ARGV[0] if defined $ARGV[0]; 			# Read from the commandline
 my $roundDownTo = 2;						# This variable prevents questions that are too simple, like "0 x 1" & "1 X 6"
-print "Subject: Products to $productsTo\n\n";
+print "Products to $maxProduct\n";
+print "Division questions to $maxDiv\n" if defined $maxDiv;
+print "\n";
+
 my ($maxQuestions, $randA, $randB ); 
 for ($maxQuestions = 1; $maxQuestions<31; $maxQuestions++) {
 	do {
 		# New requirement: questions like 3x16=38 are "too hard".  The rubric says "hardest question: 7x7".
-		#$randA = int(rand($productsTo));
-		#$randB = int(rand($productsTo));
 		$randA = int(rand(9));
 		$randB = int(rand(9));
-	} while (($randA * $randB >= $productsTo) || $randA < $roundDownTo || $randB < $roundDownTo );
+	} while (($randA * $randB >= $maxProduct) || $randA < $roundDownTo || $randB < $roundDownTo );
 
-	print "$maxQuestions. $randA x $randB =\n";
+	if ((defined $maxDiv) and (($randA * $randB) <= $maxDiv)) { 
+		print "$maxQuestions. " . $randA * $randB .  " % $randB =\n";
+	} else {
+		print "$maxQuestions. $randA x $randB =\n";
+	}
 
 }
