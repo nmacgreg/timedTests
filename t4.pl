fcd7969ec74c46c1e1a9c954e85bd2523ccd5f25
BuildTools <unconfigured@null.spigotmc.org>
2019-02-21 00:35:36 -0700

    t4.pl tweaked, replacing the hardcoded 9 with the maxQuotient variable, for levels 19 and above

--- t4.pl
+++ t4.pl
@@ -3,6 +3,7 @@
 
 #use taint; 
 use strict; 
+use POSIX;
 use Getopt::Long; 
 my $maxProduct = 42;  						# Declare a variable & set a default
 my $maxDiv;
@@ -17,7 +18,7 @@ print "Products to $maxProduct\n";
 print "Division questions to $maxDiv\n" if defined $maxDiv;
 print "\n";
 
-my $maxQuotient = sqrt $maxProduct;  # figure out which times-table we're focusing on, in this test
+my $maxQuotient = floor sqrt $maxProduct;  # figure out which times-table we're focusing on, in this test
 print "$maxQuotient times tables\n";
 
 my ($maxQuestions, $randA, $randB, $randC, $randD ); 
@@ -25,13 +26,13 @@ for ($maxQuestions = 1; $maxQuestions<31; $maxQuestions++) {
 	do {
 		# New requirement: questions like 3x16=38 are "too hard".  The rubric says "hardest question: 7x7".
 		$randA = $maxQuotient;  
-		$randB = int(rand(9));
+		$randB = int(rand($maxQuotient));
 	} while (($randA * $randB >= $maxProduct) || $randA < $roundDownTo || $randB < $roundDownTo );
 
 	if ((defined $maxDiv) and (($randA * $randB) <= $maxDiv)) { 
-		$randC=int(rand(2)); # select addition or multiplication
+		$randC=int(rand(2)); # select division or multiplication
 		if ( $randC == 1  ) {
-			print "$maxQuestions. " . $randA * $randB .  " ÷ 9 =\n";
+			print "$maxQuestions. " . $randA * $randB .  " ÷ $maxQuotient =\n";
 		}else {
 			print "$maxQuestions. $randA x $randB =\n";
 		}
